5d230a49fdb620ee96eec0c36c1ac9d6c5dcecae
lichengcan <li929825769@163.com>
2023-11-30 18:07:23 +0800

    密码模式

--- lab-68-spring-security-oauth/lab-68-demo02-authorization-server-with-resource-owner-password-credentials/src/main/java/cn/iocoder/springboot/lab68/authorizationserverdemo/config/OAuth2AuthorizationServerConfig.java
+++ lab-68-spring-security-oauth/lab-68-demo02-authorization-server-with-resource-owner-password-credentials/src/main/java/cn/iocoder/springboot/lab68/authorizationserverdemo/config/OAuth2AuthorizationServerConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
  * 授权服务器配置
  */
 @Configuration
+//启用授权服务器功能
 @EnableAuthorizationServer
 public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
@@ -22,11 +23,27 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    /**
+     * 授权服务器的端点
+     * 设置了认证管理器用于支持密码模式的授权码获取
+     * 在密码模式中，客户端将提交用户名和密码，授权服务器将使用AuthenticationManager来验证这些凭据是否有效。
+     * 将注入的AuthenticationManager对象设置到授权服务器的端点配置中，以便授权服务器能够使用它来处理各种类型的授权请求，确保客户端的身份验证过程安全有效。
+     * @param endpoints
+     * @throws Exception
+     */
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.authenticationManager(authenticationManager);
     }
 
+    /**
+     * 设置授权服务器的安全性规则，确保只有经过身份验证的用户才能使用有效的访问令牌来访问受保护的资源。
+     * 确保只有经过成功认证的用户（已登录用户）才能使用有效的访问令牌来进行受保护资源的访问。如果用户没有通过身份验证，即使拥有有效的访问令牌，也无法访问受保护的资源。
+     * checkTokenAccess()方法接受一个字符串参数，用于指定访问令牌的安全性规则。在这里，使用的规则是isAuthenticated()，它表示只有经过身份验证的用户才能访问令牌
+     * permitAll()，它表示所有用户都被允许访问令牌密钥。
+     * @param oauthServer
+     * @throws Exception
+     */
     @Override
     public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
         oauthServer.checkTokenAccess("isAuthenticated()");
@@ -36,6 +53,12 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
 //                .checkTokenAccess("permitAll()");
     }
 
+    /**
+     * 配置了客户端的信息、内存存储方式
+     * 指定了一个名为 "clientapp" 的客户端，设置了它的密码、授权方式（这里是密码模式）、以及作用域（scope）。
+     * @param clients
+     * @throws Exception
+     */
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
--- lab-68-spring-security-oauth/lab-68-demo02-authorization-server-with-resource-owner-password-credentials/src/main/java/cn/iocoder/springboot/lab68/authorizationserverdemo/config/SecurityConfig.java
+++ lab-68-spring-security-oauth/lab-68-demo02-authorization-server-with-resource-owner-password-credentials/src/main/java/cn/iocoder/springboot/lab68/authorizationserverdemo/config/SecurityConfig.java
@@ -10,20 +10,36 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
+//启用Web安全模块
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    /**
+     * 身份验证管理器（AuthenticationManager）
+     * @return
+     * @throws Exception
+     */
     @Override
     @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
 
+    /**
+     * 密码编码器
+     * @return
+     */
     @Bean
     public static NoOpPasswordEncoder passwordEncoder() {
         return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
     }
 
+    /**
+     * 通过重写configure(AuthenticationManagerBuilder auth)方法
+     * 使用内存中的用户详细信息管理器（InMemoryUserDetailsManager）来配置用户信息，并指定密码编码器为上一步注册的NoOpPasswordEncoder。
+     * @param auth
+     * @throws Exception
+     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.
